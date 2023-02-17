be9b638cfdbbd6c7c060e8fc9030da18f8a5489a
Muhammad Usman Shakeel <ushakeel808@gmail.com>
2023-02-18 03:06:43 +0500

    3rd Commit

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from revChatGPT.V1 import Chatbot
-
+import pandas as pd 
+import numpy as np 
+import os 
 bot = Chatbot(config = {
-    "email": "xxx@xxx",
-    "password": "xxxx"
+    "email": "eyolveofficial@gmail.com",
+    "password": "eyolve1234"
 })
+df = pd.read_csv("CleanData.csv")
+messages = df['message'].values
+emails = df['From[Mail]'].values
+classes = {"IsPromotional":[],
+           "FromMail":[]}
 
-prompt = "Is this message promotional? (Limited time offer! Get 20% off all items in our store today. Don't miss out!) answer in Yes/No only"
-for data in bot.ask(prompt):
-    response = data['message']
+for index, (mail,msg) in enumerate(zip(emails,messages)):
+    prompt = "Is this message promotional? ({}) answer in Yes/No only".format(msg)
+    print(f"Checking Message # {index+1}")
+    for data in bot.ask(prompt):
+        response = data['message']
+    if 'yes' in response.lower():
+        classes['IsPromotional'].append(1)
+        
+    if 'no' in response.lower():
+        classes['IsPromotional'].append(1)
+    else:
+        classes['IsPromotional'].append(-1)
+    classes['FromMail'].append(mail)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print(response)
+
+
+pd.DataFrame(classes).to_csv("MailValidation.csv",index=False)
+
+
+# for i in range(1000):
+
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     print(f"Checked Message # {i}")
